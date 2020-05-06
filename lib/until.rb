1416a83a58ae53e400794f1d2ae0bb43b83f0fff
Vianka Rivera <viankalrivera@gmail.com>
2020-05-06 01:56:15 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -5,13 +5,3 @@ def using_until
       levitation_force +=1 
   end    
 end
-
-#def using_while
-  levitation_force = 6
-	
-	
-	while levitation_force <10 
-	  puts "Wingardium Leviosa"
-	   levitation_force += 1 
-	 end 
-	end
